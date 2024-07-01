048ec3fe7d3095388a47d43ff7d8fac14ce686dc
SkulkRangerMC-Dev <skulrangermc@gmail.com>
2024-07-01 18:05:42 +0530

    Biome help needed 3

--- src/main/java/sculkrange/faraway/arctic/FarawayArcticEscapeDataGenerator.java
+++ src/main/java/sculkrange/faraway/arctic/FarawayArcticEscapeDataGenerator.java
@@ -3,11 +3,13 @@ package sculkrange.faraway.arctic;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import sculkrange.faraway.arctic.datagen.ModItemTagProvider;
+import sculkrange.faraway.arctic.datagen.ModWorldGenerator;
 
 public class FarawayArcticEscapeDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModWorldGenerator::new);
 	}
 }
